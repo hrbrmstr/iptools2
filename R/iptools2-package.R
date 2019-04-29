95b6a625108ed921e1d4587572eb33d166e15e34
hrbrmstr <bob@rud.is>
2019-04-29 15:27:23 -0400

    R package repo initialization complete

---
+++ R/iptools2-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/iptools2>
+#' - BugReports: <https://gitlab.com/hrbrmstr/iptools2/issues>
+#' 
+#' @md
+#' @name iptools2
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
